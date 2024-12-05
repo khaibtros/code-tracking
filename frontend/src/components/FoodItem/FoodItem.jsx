65f3be14ce8c6e7a011747cdab8c61773a761cf1
khaibtros <trankhai15052005@gmail.com>
2024-12-05 16:39:11 +0700

    apply CSS for displaying food list, add food item Counter

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/frontend_assets/assets'
 
 const FoodItem = ({id,name,description,price,image}) => {
+
+    const [itemCount,setItemCount] = useState(0);
+
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
             <img src={image} alt="" className="food-item-image" />
+            {!itemCount 
+                ? <img class="add" onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" />
+                : <div className='food-item-counter'>
+                    <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+                    <p>{itemCount}</p>
+                    <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+                </div>
+            }
         </div>
         <div className="food-item-info">
             <div className="food-item-name-rating">
