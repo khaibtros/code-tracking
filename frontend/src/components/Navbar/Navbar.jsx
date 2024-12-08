50524162b4d2944e208e60faee7c04bd15b147c0
khaibtros <trankhai15052005@gmail.com>
2024-12-09 01:16:30 +0700

    Make Navbar links work

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/frontend_assets/assets'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -11,10 +12,10 @@ const Navbar = () => {
     <div className="navbar">
         <img src={assets.logo} alt="Logo" className="logo"/>
         <ul className="navbar-menu">
-            <li className={menu === "home" ? "active" : ""} onClick={() => setMenu("home")}>home</li>
-            <li className={menu === "menu" ? "active" : ""} onClick={() => setMenu("menu")}>menu</li>
-            <li className={menu === "mobile-app" ? "active" : ""} onClick={() => setMenu("mobile-app")}>mobile-app</li>
-            <li className={menu === "contact-us" ? "active" : ""} onClick={() => setMenu("contact-us")}>contact us</li>
+            <Link to="/" className={menu === "home" ? "active" : ""} onClick={() => setMenu("home")}>home</Link>
+            <a href="#explore-menu" className={menu === "menu" ? "active" : ""} onClick={() => setMenu("menu")}>menu</a>
+            <a href="#app-download" className={menu === "mobile-app" ? "active" : ""} onClick={() => setMenu("mobile-app")}>mobile-app</a>
+            <a href="#footer" className={menu === "contact-us" ? "active" : ""} onClick={() => setMenu("contact-us")}>contact us</a>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="Search" />
