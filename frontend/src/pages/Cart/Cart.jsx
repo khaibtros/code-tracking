7a99b1530367c77510e3e44d3fae4486a035c321
khaibtros <trankhai15052005@gmail.com>
2024-12-10 20:06:18 +0700

    Add HTML for the Cart Totals section

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -19,7 +19,7 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item,index) => {
+        {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
               <div>
@@ -29,14 +29,44 @@ const Cart = () => {
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>${item.price * cartItems[item._id]}</p>
-                  <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
                 </div>
                 <hr />
               </div>
             )
           }
-        })
-        }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+            <h2>Cart Totals</h2>
+            <div>
+              <div className="cart-total-details">
+                <p>Subtotal</p>
+                <p>{0}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details">
+                <p>Delivery Fee</p>
+                <p>{2}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details">
+                <p>Total</p>
+                <p>{0}</p>
+              </div>
+              <button>PROCEED TO CHECKOUT</button>
+          </div>
+          <div className="cart-promocode">
+            <div>
+              <p>If you have a promo code, Enter it here</p>
+              <div className="cart-promocode-input">
+                <input type="text" placeholder='promo-code' />
+                <button>Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
