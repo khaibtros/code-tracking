8dbd36596f50732e6b613fa2f47a29c2ea8aaa17
khaibtros <trankhai15052005@gmail.com>
2024-12-10 21:35:04 +0700

    Apply CSS for PlaceOrder page

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -4,30 +4,30 @@ import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
 
-  const {getTotalCartAmount} = useContext(StoreContext);
+  const { getTotalCartAmount } = useContext(StoreContext);
 
   return (
     <form className='place-order'>
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input type="text" placeholder='First name'/>
-          <input type="text" placeholder='Last name'/>
+          <input type="text" placeholder='First name' />
+          <input type="text" placeholder='Last name' />
         </div>
-        <input type="email" placeholder='Email address'/>
-        <input type="text" placeholder='Street'/>
+        <input type="email" placeholder='Email address' />
+        <input type="text" placeholder='Street' />
         <div className="multi-fields">
-          <input type="text" placeholder='City'/>
-          <input type="text" placeholder='State'/>
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder='Zip code'/>
-          <input type="text" placeholder='Country'/>
+          <input type="text" placeholder='Zip code' />
+          <input type="text" placeholder='Country' />
         </div>
-        <input type="text" placeholder='Phone'/>
+        <input type="text" placeholder='Phone' />
       </div>
       <div className="place-order-right">
-      <div className="cart-total">
+        <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
             <div className="cart-total-details">
@@ -42,7 +42,7 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()+2}</b>
+              <b>${getTotalCartAmount() + 2}</b>
             </div>
           </div>
           <button>PROCEED TO PAYMENT</button>
