34d4446f7099c13e945fabe63880e71c44f8cb1d
khaibtros <trankhai15052005@gmail.com>
2024-12-10 20:59:33 +0700

    get getTotalCartAmount > 0 to display dot on the basket icon in the Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/frontend_assets/assets'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("home");
+    const {getTotalCartAmount} = useContext(StoreContext); 
 
   return (
     <div className="navbar">
@@ -21,7 +23,7 @@ const Navbar = ({setShowLogin}) => {
             <img src={assets.search_icon} alt="Search" />
             <div className="navbar-basket-icon">
                 <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-                <div className="dot"></div>
+                <div className={getTotalCartAmount()===0?"":"dot"}></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>Sign in</button>
         </div>
